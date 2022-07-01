a032d77713ccaef5dcb771d268e12e76c2b4dbfb
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-07-01 17:01:26 -0400

    Workflow for GPM FWI data

--- FWI.GPM.LATE.v5/01-create-zarr.py
+++ FWI.GPM.LATE.v5/01-create-zarr.py
@@ -5,13 +5,19 @@ import re
 
 import xarray as xr
 import pandas as pd
-import zarr
+import numpy as np
 
 from dask.diagnostics.progress import ProgressBar
 
-prefix = "FWI_GPM_LATE_v5_Daily-test"
-files = sorted(list(glob.glob("raw-input/2022/*.nc")))
-# files = files[0:20]
+prefix = "FWI_GPM_LATE_v5_Daily"
+files = sorted(list(glob.glob("raw-input/*/*.Daily.*.nc")))
+# files = files[:-3]
+
+chunking = {
+    "time": 50,
+    "lat": 100,
+    "lon": 100
+}
 
 def parse_date(path):
     ms = re.search(r"\.(\d{4})(\d{2})(\d{2})\.nc$", path)
@@ -21,18 +27,38 @@ def parse_date(path):
     date = datetime.date(year, month, day)
     return date
 
+print("Reading dataset...")
 dat_raw = xr.open_mfdataset(files, combine="nested", concat_dim="time")
 # The time coordinate here is wrong, so we reassign it by parsing the file name
+print("Fixing time coordinate...")
 dates = pd.to_datetime([parse_date(f) for f in files])
-dat = dat_raw.assign_coords(time=dates).chunk({
-    "time": 100,
-    "lat": 100,
-    "lon": 100
-})
+dat = dat_raw.assign_coords(time=dates)
 
+ntime = len(dat.time)
+residual_chunks = chunking["time"] - (ntime % chunking["time"])
+
+print("Extending time series for even chunking...")
+# dummy = xr.full_like(dat.isel(time = slice(residual_chunks)), np.nan)
+# assert len(dummy.time) == residual_chunks
+newdates = pd.date_range(
+    dates.max() + pd.Timedelta(1, 'D'),
+    dates.max() + pd.Timedelta(residual_chunks, 'D')
+)
+assert len(newdates) == residual_chunks
+alldates = np.concatenate((dates, newdates))
+assert len(alldates) % chunking["time"] == 0
+dat_extended = dat.reindex({"time": alldates}, fill_value = np.nan)
+assert len(dat_extended.time) % chunking["time"] == 0
+dat_extended = dat_extended.chunk(chunking)
+
+print("Writing output zarr...")
 target = f"{prefix}.zarr"
 with ProgressBar():
-    dat.to_zarr(target, mode="w")
+    dat_extended.to_zarr(target, mode="w")
 
-print("testing zarr")
+with open("processed-files.txt", "w") as f:
+    f.writelines(files)
+
+print("Testing zarr")
 dtest = xr.open_zarr(target)
+print("Done!")
--- FWI.GPM.LATE.v5/02-update-zarr.py
+++ FWI.GPM.LATE.v5/02-update-zarr.py
@@ -3,13 +3,14 @@ import re
 
 import xarray as xr
 import pandas as pd
-import zarr
+import numpy as np
 
 timevar = "time"
 zarrpath = "FWI_GPM_LATE_v5_Daily-test.zarr"
-infile = "raw-input/2022/FWI.GPM.LATE.v5.Daily.Default.20220121.nc"
+# zarrpath = target
+infile = "raw-input/2022/FWI.GPM.LATE.v5.Daily.Default.20220630.nc"
 
-lastday = xr.open_zarr(zarrpath)[timevar].max().values
+# lastday = xr.open_zarr(zarrpath)[timevar].max().values
 
 def parse_date(path):
     ms = re.search(r"\.(\d{4})(\d{2})(\d{2})\.nc$", path)
@@ -25,11 +26,40 @@ dnew = dnew_raw\
     .drop("time")\
     .expand_dims(time=[pd.to_datetime(parse_date(infile))], axis=0)
 
-dnew.to_zarr(zarrpath, append_dim="time")
-zarr.consolidate_metadata(zarrpath)
+# Is the current timestamp in the Zarr file?
+dtarget = xr.open_zarr(zarrpath)
+is_in = dtarget.time.isin(dnew.time)
+n_is_in = is_in.sum()
+if n_is_in > 1:
+    raise Exception(f"Found {n_is_in} matching times! Something is wrong with this situation.")
+elif n_is_in == 1:
+    # Yes! Where exactly?
+    itime = int(np.where(is_in)[0])
+    # Write to that exact location (replace NaNs with new data)
+    dnew.to_zarr(zarrpath, region={
+        "time": slice(itime, itime+1),
+        "lat": slice(0, dnew.sizes["lat"]),
+        "lon": slice(0, dnew.sizes["lon"])
+    })
+elif n_is_in == 0:
+    # No, so we need to extend the time series.
+    # For performance, we extend by one full time chunksize -- `tchunk`
+    tchunk = dtarget.chunks["time"][-1]
+    newdates = pd.date_range(
+        dtarget.time.max().values + pd.Timedelta(1, 'D'),
+        dtarget.time.max().values + pd.Timedelta(tchunk, 'D')
+    )
+    assert len(newdates) == len(tchunk)
+    # Extend the current timestep out to size `tchunk`, filling with `Nan`s
+    dummy = dnew.reindex({"time": newdates}, fill_value=np.nan)
+    assert len(dummy.time) == len(tchunk)
+    # Now, append to the existing Zarr data store
+    dummy.to_zarr(zarrpath, append_dim="time")
 
-dtest = xr.open_zarr(zarrpath, consolidated=False)
-dtest.chunks
+# Test the result
+print("Testing new Zarr...")
+dtest = xr.open_zarr(zarrpath)
+print("Done!")
 
 # Update algorithm
 # (1) On creation, fill the last time dimension to the end with `nan`s
